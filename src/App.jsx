85ef09cd02003c4efa54b49969a22dab01f9925f
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-13 15:34:38 -0600

    added styling to page

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ import { useState } from 'react'
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null)
-
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
-    <>
-      { featPupId && <p>{ featPupId }</p> }
+    <div className='main'>
+      <section>
+            {puppies.map((puppy)=> {
+              return <p onClick={()=>{setFeatPupId(puppy.id);
+              }} key={puppy.id}>{puppy.name}</p>
+            })}
+      </section>
 
-      {puppies.map((puppy)=> {
-        return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
-      })}
-    </>
+      {featPupId && (
+              <div className='detail'>
+                <h2>{featuredPup.name}</h2>
+                <ul>
+                  <li>Age: {featuredPup.age}</li>
+                  <li>Email: {featuredPup.email}</li>
+                </ul>
+              </div>
+            )}
+
+      
+    </div>
   )
 }
 
