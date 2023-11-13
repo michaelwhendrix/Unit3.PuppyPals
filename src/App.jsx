ec849e038f8a537cf1037dfabbccadfa43d911b9
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-13 14:12:39 -0600

    added state variable for puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 
+  console.log(puppies);
   return (
     <>
-      
+      {puppies.map((puppy)=> {
+        return <p onClick={()=>{}} key={puppy.id}>{puppy.name}</p>
+      })}
     </>
   )
 }
